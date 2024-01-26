31fd1515d1f19e6af96fb42c93a5ddf9c1ade37b
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-27 00:14:08 +0100

    Load achievements

--- LethalAchievements/LethalAchievements.cs
+++ LethalAchievements/LethalAchievements.cs
@@ -2,9 +2,11 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using LethalAchievements.Features;
 
 namespace LethalAchievements;
 
+[BepInDependency("LethalEventsLib")]
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class LethalAchievements : BaseUnityPlugin
 {
@@ -12,12 +14,17 @@ public class LethalAchievements : BaseUnityPlugin
     private Harmony? Harmony { get; set; }
     internal new static ManualLogSource? Logger { get; private set; }
     public static LethalAchievements? Instance { get; private set; }
+
+    // Config entries
     public ConfigEntry<bool>? PlaceHolder { get; private set; }
 
     private void Awake()
     {
         // Set instance
         Instance = this;
+        
+        // Set do not destroy on load
+        DontDestroyOnLoad(this);
 
         // Init logger
         Logger = base.Logger;
@@ -30,6 +37,10 @@ public class LethalAchievements : BaseUnityPlugin
 
         // Plugin startup logic
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        
+        // Load achievements
+        // TODO: This needs to be done after other plugins have loaded --> Event in LethalEventsLib when main menu is loaded?
+        AchievementManager.FindAllAchievements();
     }
 
     public void PatchAll()
