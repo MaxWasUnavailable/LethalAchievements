bd32e9ef7a4a2a13a3b6b9a64eee8267b2fd3b0c
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-26 16:37:53 +0100

    Initial commit

---
+++ LethalAchievements/LethalAchievements.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LethalAchievements;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class LethalAchievements : BaseUnityPlugin
+{
+    private bool _isPatched;
+    private Harmony Harmony { get; set; }
+    private new static ManualLogSource Logger { get; set; }
+    public static LethalAchievements Instance { get; private set; }
+    public ConfigEntry<bool> PlaceHolder { get; private set; }
+
+    private void Awake()
+    {
+        // Set instance
+        Instance = this;
+
+        // Init logger
+        Logger = base.Logger;
+
+        // Init config entries
+        PlaceHolder = Config.Bind("General", "PlaceHolder", true, "PlaceHolder");
+
+        // Patch using Harmony
+        PatchAll();
+
+        // Report plugin is loaded
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public void PatchAll()
+    {
+        if (_isPatched)
+        {
+            Logger.LogWarning("Already patched!");
+            return;
+        }
+
+        Logger.LogDebug("Patching...");
+
+        Harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        Harmony.PatchAll();
+        _isPatched = true;
+
+        Logger.LogDebug("Patched!");
+    }
+
+    public void UnpatchAll()
+    {
+        if (!_isPatched)
+        {
+            Logger.LogWarning("Already unpatched!");
+            return;
+        }
+
+        Logger.LogDebug("Unpatching...");
+
+        Harmony.UnpatchSelf();
+        _isPatched = false;
+
+        Logger.LogDebug("Unpatched!");
+    }
+}
