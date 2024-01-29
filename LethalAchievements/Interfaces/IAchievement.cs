3e41301eaef620589657d27c3a2c9891aa885783
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-29 15:15:49 +0100

    Add optional sprite

--- LethalAchievements/Interfaces/IAchievement.cs
+++ LethalAchievements/Interfaces/IAchievement.cs
@@ -1,14 +1,16 @@
 using System;
+using UnityEngine;
 
 namespace LethalAchievements.Interfaces;
 
 public interface IAchievement
 {
-    event Action? AchievedEvent;
     public string Name { get; internal set; }
     public string? Description { get; internal set; }
     public bool IsAchieved { get; internal set; }
     public bool IsHidden { get; internal set; }
+    public Sprite? Icon { get; internal set; }
+    event Action? AchievedEvent;
 
     public void Initialize();
     public void Uninitialize();
