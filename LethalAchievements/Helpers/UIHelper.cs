0ac9d38b94a3c20222584b3d9be81a2c2f3a757f
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-03-27 02:30:49 +0100

    Create UI helper

---
+++ LethalAchievements/Helpers/UIHelper.cs
@@ -0,0 +1,36 @@
+namespace LethalAchievements.Helpers;
+
+/// <summary>
+///     Helper class for UI-related features.
+/// </summary>
+public static class UIHelper
+{
+    /// <summary>
+    ///     Displays a status message on the HUD.
+    /// </summary>
+    /// <param name="message"> The message to display. </param>
+    public static void DisplayStatusMessage(string message)
+    {
+        HUDManager.Instance.DisplayStatusEffect(message);
+    }
+
+    /// <summary>
+    ///     Displays a tip on the HUD.
+    /// </summary>
+    /// <param name="title"> The title of the tip. </param>
+    /// <param name="message"> The message of the tip. </param>
+    /// <param name="warningStyle"> Whether the tip should be displayed in a warning style. </param>
+    public static void DisplayTip(string title, string message, bool warningStyle = false)
+    {
+        HUDManager.Instance.DisplayTip(title, message, warningStyle);
+    }
+
+    /// <summary>
+    ///     Sends a chat message to all players on behalf of the server.
+    /// </summary>
+    /// <param name="message"> The message to send. </param>
+    public static void SendServerChatMessage(string message)
+    {
+        HUDManager.Instance.AddTextToChatOnServer(message);
+    }
+}
