daf17f223af08e1bdda980d518acb6edcadf026c
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-04 16:20:48 +0200

    Fix not flagging plugins as loaded before using methods that require this

--- LethalAchievements/LethalAchievements.cs
+++ LethalAchievements/LethalAchievements.cs
@@ -49,8 +49,9 @@ public class LethalAchievements : BaseUnityPlugin
 
     private static void OnGameLoaded()
     {
+        ArePluginsLoaded = true;
+        
         // Initialize achievements system
         AchievementManager.Initialize();
-        ArePluginsLoaded = true;
     }
 }
