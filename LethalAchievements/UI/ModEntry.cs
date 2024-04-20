725aeddf5c67bc7fc4dd42421f113229e9c71d75
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-20 20:12:10 +0200

    Cleanup ModEntry

--- LethalAchievements/UI/ModEntry.cs
+++ LethalAchievements/UI/ModEntry.cs
@@ -11,49 +11,25 @@ namespace LethalAchievements.UI;
 
 internal class ModEntry : MonoBehaviour
 {
-    private GameObject Divider;
-    private TMP_Text Name;
-
-    private List<ModEntry> AllMods;
-    internal Dictionary<IAchievement, AchievementEntry> AchievementEntries = [];
-
-    private bool isExpanded = false;
+    private GameObject? Divider { get; set; }
+    private TMP_Text? Name { get; set; }
+    private List<ModEntry> AllMods { get; set; } = [];
+    internal Dictionary<IAchievement, AchievementEntry> AchievementEntries { get; } = new();
+    private bool IsExpanded { get; set; }
 
     internal void Init(string modName, List<IAchievement> achievements, List<ModEntry> mods, Transform achievementList)
     {
-        // Used to display mod name
-        Name = transform.Find("Name").GetComponent<TMP_Text>();
-        Name.text = modName.Truncate(30);
-        Name.gameObject.SetActive(false);
-        
-        // Employs seamless merging with window
-        Divider = transform.Find("Divide").gameObject;
-        Divider.gameObject.SetActive(false);
-        
-        transform.GetComponent<Button>().onClick.AddListener(() => OnClick());
-        
-        // Set mod image to one found with {name}.png
-        // Make a class for different types of icon loadings etc etc
-        // Current directory for now, CustomSounds has logic for all mods that have a folder named "Custom sounds" 
-        try
-        {
-            var iconPath = $"./{modName}.png";
-            var rawData = File.ReadAllBytes(iconPath);
-            var iconTexture = new Texture2D(64, 64);
-            iconTexture.LoadImage(rawData);
-
-            // Create sprite with texture
-            transform.Find("Icon").GetComponent<Image>().sprite = Sprite.Create(iconTexture,
-                new Rect(0, 0, iconTexture.width, iconTexture.height), Vector2.zero);
-        }
-        catch
-        {
-            LethalAchievements.Logger?.LogDebug($"No icon found for {modName}! - Using default icon.");
-        }
-
+        InitName(modName);
+        InitDivider();
+        InitModIcon(modName);
         AllMods = mods;
+
+        // Add listener to button
+        transform.GetComponent<Button>().onClick.AddListener(OnClick);
+
         var achievementTemplate = achievementList.Find("Achievement").gameObject;
         achievementTemplate.SetActive(false);
+
         foreach (var achievement in achievements)
         {
             var achievementObj = Instantiate(achievementTemplate, achievementList);
@@ -64,20 +40,75 @@ internal class ModEntry : MonoBehaviour
         }
     }
 
+    private void InitName(string modName)
+    {
+        Name = transform.Find("Name").GetComponent<TMP_Text>();
+        Name.text = modName.Truncate(30);
+        Name.gameObject.SetActive(false);
+    }
+
+    private void InitDivider()
+    {
+        Divider = transform.Find("Divide").gameObject;
+        Divider.gameObject.SetActive(false);
+    }
+
+    private void InitModIcon(string modName)
+    {
+        // TODO: This system needs a rework
+
+        // From Nebby:
+        // Set mod image to one found with {name}.png
+        // Make a class for different types of icon loadings etc etc
+        // Current directory for now, CustomSounds has logic for all mods that have a folder named "Custom sounds" 
+        var iconPath = $"./{modName}.png";
+
+        if (!File.Exists(iconPath))
+        {
+            // Check if the mod has a folder with the same name
+            iconPath = $"./{modName}/{modName}.png";
+            if (!File.Exists(iconPath))
+            {
+                LethalAchievements.Logger?.LogDebug($"No icon found for {modName}! - Using default icon.");
+                return;
+            }
+        }
+
+        try
+        {
+            var rawData = File.ReadAllBytes(iconPath);
+            var iconTexture = new Texture2D(64, 64);
+            iconTexture.LoadImage(rawData);
+
+            // Create sprite with texture
+            transform.Find("Icon").GetComponent<Image>().sprite = Sprite.Create(iconTexture,
+                new Rect(0, 0, iconTexture.width, iconTexture.height), Vector2.zero);
+        }
+        catch
+        {
+            LethalAchievements.Logger?.LogError($"Failed to load icon for {modName}!");
+        }
+    }
+
     private void OnClick()
     {
         HUDController.ClickSFX.Play();
         // Sets any mods not clicked to not be displayed
-        if(!isExpanded) Toggle(!isExpanded);
+        if (!IsExpanded)
+            Toggle(!IsExpanded);
     }
+
     private void Toggle(bool isActive)
     {
         if (isActive)
-            foreach (var mod in AllMods.Where(mod => mod != this)) mod.Toggle(false);
-        
-        foreach (var achievement in AchievementEntries) achievement.Value.gameObject.SetActive(isActive);
-        isExpanded = isActive;
-        Name.gameObject.SetActive(isActive);
-        Divider.gameObject.SetActive(isActive);
+            foreach (var mod in AllMods.Where(mod => mod != this))
+                mod.Toggle(false);
+
+        foreach (var achievement in AchievementEntries)
+            achievement.Value.gameObject.SetActive(isActive);
+
+        IsExpanded = isActive;
+        Name?.gameObject.SetActive(isActive);
+        Divider?.gameObject.SetActive(isActive);
     }
 }
