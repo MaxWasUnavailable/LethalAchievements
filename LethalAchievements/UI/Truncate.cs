3c884219feaa90c33a4191666fc3682de7c3ae81
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-20 19:40:37 +0200

    Use range indexer

--- LethalAchievements/UI/Truncate.cs
+++ LethalAchievements/UI/Truncate.cs
@@ -5,7 +5,7 @@ internal static class StringExt
     internal static string? Truncate(this string? value, int maxLength)
     {
         return value?.Length > maxLength
-            ? value.Substring(0, maxLength)
+            ? value[..maxLength]
             : value;
     }
 }
